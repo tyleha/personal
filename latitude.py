6cdb45006bc010206ce0d0c90ac21217205d943a
Tyler Hartley <tyleha@gmail.com>
2014-05-09 16:36:51 -0700

    plotting seattle neighborhoods

--- latitude.py
+++ latitude.py
@@ -74,9 +74,14 @@ def distance_on_unit_sphere(lat1, long1, lat2, long2):
 
 # <codecell>
 
-fh = open(r'C:\Users\thartley\Documents\Dropbox\LocationHistory5_1_14.json')
+fh = open(r'C:\Users\thartley\Documents\LocationHistory5_1_14.json')
 buf = fh.read()
 raw = json.loads(buf)
+fh.close()
+
+ld = pd.DataFrame(raw['locations'])
+ld['latitudeE7'] = ld['latitudeE7']/float(1e7)
+ld['longitudeE7'] = ld['longitudeE7']/float(1e7)
 
 # <codecell>
 
@@ -85,7 +90,7 @@ point.date
 
 # <codecell>
 
-points = [Point.from_google_latitude(p) for p in raw['locations'][:1900]]
+fpoints = [Point.from_google_latitude(p) for p in raw['locations'][:1900]]
 plt.plot([point.lon for point in points], [point.lat for point in points], 'b.-')
 
 # <headingcell level=1>
@@ -95,11 +100,19 @@ plt.plot([point.lon for point in points], [point.lat for point in points], 'b.-'
 # <codecell>
 
 import fiona
+from itertools import chain
+from mpl_toolkits.basemap import Basemap
+import pandas as pd
+from shapely.geometry import Point, Polygon, MultiPoint, MultiPolygon
+from shapely.prepared import prep
 
 # <codecell>
 
+#shp = fiona.open(r'C:\Users\thartley\Documents\Seattle_20City_20Limits\WGS84\Seattle City Limits')
+#r'C:\Users\thartley\Downloads\london\london_wards'
+shapefile = r'C:\Users\thartley\Downloads\Neighborhoods\WGS84\Neighborhoods'
 
-shp = fiona.open('data/london_wards.shp')
+shp = fiona.open(shapefile+'.shp')
 bds = shp.bounds
 shp.close()
 extra = 0.01
@@ -107,4 +120,61 @@ ll = (bds[0], bds[1])
 ur = (bds[2], bds[3])
 coords = list(chain(ll, ur))
 w, h = coords[2] - coords[0], coords[3] - coords[1]
+
+# <codecell>
+
+m = Basemap(
+    projection='tmerc',
+    lon_0=-122.3,
+    lat_0=47.6,
+    #lon_0=-2.,
+    #lat_0=49.,
+    ellps = 'WGS84',
+    llcrnrlon=coords[0] - extra * w,
+    llcrnrlat=coords[1] - extra + 0.01 * h,
+    urcrnrlon=coords[2] + extra * w,
+    urcrnrlat=coords[3] + extra + 0.01 * h,
+    lat_ts=0,
+    resolution='i',
+    suppress_ticks=True)
+out = m.readshapefile(
+    #r'C:\Users\thartley\Documents\Seattle_20City_20Limits\WGS84\Seattle City Limits',
+    shapefile,
+    'seattle',
+    color='none',
+    zorder=2)
+
+# <codecell>
+
+# Plot all neighborhoods
+fig = plt.figure(figsize=(8,12))
+for nhood in m.seattle:
+    plt.plot([xx[0] for xx in nhood], [xx[1] for xx in nhood], 'b')
+
+# <codecell>
+
+m.seattle_info[60]['L_HOOD']
+
+# <codecell>
+
+# set up a map dataframe
+df_map = pd.DataFrame({
+    'poly': [Polygon(xy) for xy in m.seattle],
+    'name': })
+df_map['area_m'] = df_map['poly'].map(lambda x: x.area)
+df_map['area_km'] = df_map['area_m'] / 100000
+
+# <codecell>
+
+# Create Point objects in map coordinates from dataframe lon and lat values
+map_points = pd.Series(
+    [Point(m(mapped_x, mapped_y)) for mapped_x, mapped_y in zip(ld['longitudeE7'], 
+                                                                ld['latitudeE7'])])
+plaque_points = MultiPoint(list(map_points.values))
+wards_polygon = prep(MultiPolygon(list(df_map['poly'].values)))
+# calculate points that fall within the London boundary
+ldn_points = filter(wards_polygon.contains, plaque_points)
+
+# <codecell>
+
 
