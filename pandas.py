225ea05e101980cf915a6ebf1c548015e64e72b4
Tyler Hartley <tyleha@gmail.com>
2014-08-05 17:41:55 -0700

    pandas work on baby names

--- pandas.py
+++ pandas.py
@@ -12,11 +12,11 @@ import helpers
 
 # <codecell>
 
-fn = 'usagov_bitly_data2013-05-17-1368832207'
-fpath = helpers.user_prefix()+'Downloads\\'+fn
 
 # <codecell>
 
+fn = 'usagov_bitly_data2013-05-17-1368832207'
+fpath = helpers.user_prefix()+'Downloads\\'+fn
 print fpath
 
 # <codecell>
@@ -34,12 +34,51 @@ cframe = frame[frame['a'].notnull()]
 
 # <codecell>
 
-['Android' in cf for cf in cframe['a'][:20]]
-np.where(cframe['a'])
+operating_system = np.where(cframe['a'].str.contains('Android'), 'Android', 'Other')
 
 # <codecell>
 
-np.where(cframe['a'].str.contains('Android'), 'Android', 'Other')
+operating_system.value_counts()
+
+# <codecell>
+
+by_tz_os = cframe.groupby(['tz', operating_system])
+
+# <codecell>
+
+res = by_tz_os.size().unstack(level=0).fillna(0)
+
+# <codecell>
+
+print res
+
+# <headingcell level=1>
+
+# Name Data
+
+# <codecell>
+
+years = range(1880, 2014)
+bits = list()
+folder = r'C:\Users\thartley\Downloads\names'
+
+for year in years:
+    fn = folder+'\\'+'yob%s.txt'%year
+    frame = pd.read_csv(fn, names=['name', 'sex', 'births'])
+    frame['year'] = int(year)
+    
+    bits.append(frame)
+    
+names = pd.concat(bits, ignore_index=True)
+    
+
+# <codecell>
+
+total_births = names.pivot_table(values='births', columns=['sex'], index=['year'], aggfunc=np.sum)
+
+# <codecell>
+
+total_births.plot()
 
 # <codecell>
 
