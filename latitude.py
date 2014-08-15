ea7a557216b0d5d68e14697ba3764937acdcaf10
Tyler Hartley <tyleha@gmail.com>
2014-08-15 16:32:44 -0700

    fully functional hexbin latitude data map. We did it!!

--- latitude.py
+++ latitude.py
@@ -126,107 +126,6 @@ import helpers
 
 # <codecell>
 
-try:
-    fh = open(r'C:\Users\Tyler\Documents\My Dropbox\DOCUMENTS\LocationHistory5_1_14.json')
-except:
-    fh = open(r'C:\Users\thartley\Downloads\LocationHistory_5_16_14.json')
-buf = fh.read()
-raw = json.loads(buf)
-fh.close()
-
-ld = pd.DataFrame(raw['locations'])
-ld['latitudeE7'] = ld['latitudeE7']/float(1e7)
-ld['longitudeE7'] = ld['longitudeE7']/float(1e7)
-
-# <codecell>
-
-#shp = fiona.open(r'C:\Users\thartley\Documents\Seattle_20City_20Limits\WGS84\Seattle City Limits')
-#r'C:\Users\thartley\Downloads\london\london_wards'
-shapefilename = helpers.user_prefix() + r'data\Neighborhoods\WGS84\Neighborhoods'
-#shapefilename = helpers.user_prefix() + r'data\Shorelines\WGS84\Shorelines'
-
-shp = fiona.open(shapefilename+'.shp')
-bds = shp.bounds
-shp.close()
-extra = 0.01
-ll = (bds[0], bds[1])
-ur = (bds[2], bds[3])
-coords = list(chain(ll, ur))
-w, h = coords[2] - coords[0], coords[3] - coords[1]
-
-# <codecell>
-
-m = Basemap(
-    projection='tmerc',
-    lon_0=-122.3,
-    lat_0=47.6,
-    #lon_0=-2.,
-    #lat_0=49.,
-    ellps = 'WGS84',
-    llcrnrlon=coords[0] - extra * w,
-    llcrnrlat=coords[1] - extra + 0.01 * h,
-    urcrnrlon=coords[2] + extra * w,
-    urcrnrlat=coords[3] + extra + 0.01 * h,
-    lat_ts=0,
-    resolution='i',
-    suppress_ticks=True)
-out = m.readshapefile(
-    shapefilename,
-    'seattle',
-    color='none',
-    zorder=2)
-
-m2 = Basemap(
-    projection='tmerc',
-    lon_0=-122.3,
-    lat_0=47.6,
-    #lon_0=-2.,
-    #lat_0=49.,
-    ellps = 'WGS84',
-    llcrnrlon=coords[0] - extra * w,
-    llcrnrlat=coords[1] - extra + 0.01 * h,
-    urcrnrlon=coords[2] + extra * w,
-    urcrnrlat=coords[3] + extra + 0.01 * h,
-    lat_ts=0,
-    resolution='i',
-    suppress_ticks=True)
-out = m2.readshapefile(
-    helpers.user_prefix() + r'data\Shorelines\WGS84\Shorelines',
-    'water',
-    color='none',
-    zorder=2)
-
-# <codecell>
-
-# Plot all neighborhoods
-for nhood in m.seattle:
-    plt.plot([xx[0] for xx in nhood], [xx[1] for xx in nhood], 'b')
-#plt.plot(ld['latitudeE7'], ld['longitudeE7'])
-plt.axis('scaled')
-
-# <codecell>
-
-# set up a map dataframe
-df_map = pd.DataFrame({
-    'poly': [Polygon(xy) for xy in m.seattle],#+[Polygon(yy) for yy in m2.water],
-    'name': [nhood['L_HOOD'] for nhood in m.seattle_info]})
-df_map['area_m'] = df_map['poly'].map(lambda x: x.area)
-df_map['area_km'] = df_map['area_m'] / 100000
-#df_map['poly'].append(Polygon(m2.water[0]))
-
-# <codecell>
-
-# Create Point objects in map coordinates from dataframe lon and lat values
-map_points = pd.Series(
-    [Point(m(mapped_x, mapped_y)) for mapped_x, mapped_y in zip(ld['longitudeE7'], 
-                                                                ld['latitudeE7'])])
-plaque_points = MultiPoint(list(map_points.values))
-wards_polygon = prep(MultiPolygon(list(df_map['poly'].values)))
-# calculate points that fall within the London boundary
-ldn_points = filter(wards_polygon.contains, plaque_points)
-
-# <codecell>
-
 # Convenience functions for working with colour ramps and bars
 def colorbar_index(ncolors, cmap, labels=None, **kwargs):
     """
@@ -268,66 +167,62 @@ def cmap_discretize(cmap, N):
         cdict[key] = [(indices[i], colors_rgba[i - 1, ki], colors_rgba[i, ki]) for i in xrange(N + 1)]
     return matplotlib.colors.LinearSegmentedColormap(cmap.name + "_%d" % N, cdict, 1024)
 
-# <codecell>
+# <headingcell level=4>
 
-# draw ward patches from polygons
-df_map['patches'] = df_map['poly'].map(lambda x: PolygonPatch(
-    x,
-    fc='#555555',
-    ec='#787878', lw=.25, alpha=.9,
-    zorder=4))
-
-#plt.clf()
-fig = plt.figure()
-ax = fig.add_subplot(111, axisbg='w', frame_on=False)
-
-# we don't need to pass points to m() because we calculated using map_points and shapefile polygons
-dev = m.scatter(
-    [geom.x for geom in ldn_points],
-    [geom.y for geom in ldn_points],
-    5, marker='o', lw=.25,
-    facecolor='#33ccff', edgecolor='w',
-    alpha=0.9, antialiased=True,
-    label='Blue Plaque Locations', zorder=3)
-# plot boroughs by adding the PatchCollection to the axes instance
-ax.add_collection(PatchCollection(df_map['patches'].values, match_original=True))
-# copyright and source data info
-smallprint = ax.text(
-    1.03, 0,
-    'Text',
-    ha='right', va='bottom',
-    size=4,
-    color='#555555',
-    transform=ax.transAxes)
-# Draw a map scale
-m.drawmapscale(
-    coords[0] + 0.08, coords[1] + 0.015,
-    coords[0], coords[1],
-    10.,
-    barstyle='fancy', labelstyle='simple',
-    fillcolor1='w', fillcolor2='#555555',
-    fontcolor='#555555',
-    zorder=5)
-plt.title("Tyler's Location")
-plt.tight_layout()
-# this will set the image width to 722px at 100dpi
-fig.set_size_inches(52.5, 72.2)
-plt.savefig(helpers.user_prefix()+'data\london_plaques.png', dpi=100, alpha=True)
-plt.show()
+# Load Latitude data
 
 # <codecell>
 
-#fig.set_size_inches(10, 25)
-plt.savefig(helpers.user_prefix()+'data\london_plaques.png', dpi=100, alpha=True)
-plt.show()
+try:
+    fh = open(r'C:\Users\Tyler\Documents\My Dropbox\DOCUMENTS\LocationHistory5_1_14.json')
+except:
+    fh = open(r'C:\Users\thartley\Downloads\LocationHistory_5_16_14.json')
+buf = fh.read()
+raw = json.loads(buf)
+fh.close()
 
-# <headingcell level=1>
+ld = pd.DataFrame(raw['locations'])
+ld['latitudeE7'] = ld['latitudeE7']/float(1e7)
+ld['longitudeE7'] = ld['longitudeE7']/float(1e7)
 
-# Scratch
+# <headingcell level=4>
+
+# Get boundaries from shapefile
+
+# <codecell>
+
+#shp = fiona.open(r'C:\Users\thartley\Documents\Seattle_20City_20Limits\WGS84\Seattle City Limits')
+#r'C:\Users\thartley\Downloads\london\london_wards'
+shapefilename = helpers.user_prefix() + r'data\Neighborhoods\WGS84\Neighborhoods'
+#shapefilename = helpers.user_prefix() + r'data\Shorelines\WGS84\Shorelines'
+
+shp = fiona.open(shapefilename+'.shp')
+bds = shp.bounds
+shp.close()
+extra = 0.01
+ll = (bds[0], bds[1])
+ur = (bds[2], bds[3])
+coords = list(chain(ll, ur))
+w, h = coords[2] - coords[0], coords[3] - coords[1]
 
 # <codecell>
 
 m = Basemap(
+    projection='tmerc',
+    lon_0=np.mean([coords[0], coords[2]]),
+    lat_0=np.mean([coords[1], coords[3]]),
+    ellps = 'WGS84',
+    llcrnrlon=coords[0] - extra * w,
+    llcrnrlat=coords[1] - (0.08 * h), 
+    urcrnrlon=coords[2] + extra * w,
+    urcrnrlat=coords[3] + (extra+0.01 * h),
+    resolution='i',
+    suppress_ticks=True)
+# Import the shapefile data to the Basemap 
+out = m.readshapefile(shapefilename, 'seattle', drawbounds=False, color='none', zorder=2)
+
+zzz ="""
+m2 = Basemap(
     projection='tmerc',
     lon_0=-122.3,
     lat_0=47.6,
@@ -341,12 +236,103 @@ m = Basemap(
     lat_ts=0,
     resolution='i',
     suppress_ticks=True)
-out = m.readshapefile(
-    r'C:\Users\Tyler\data\Shorelines\WGS84\Shorelines',
-    
-    'seattle',
+out = m2.readshapefile(
+    helpers.user_prefix() + r'data\Shorelines\WGS84\Shorelines',
+    'water',
     color='none',
     zorder=2)
+"""
+
+# <codecell>
+
+# set up a map dataframe
+df_map = pd.DataFrame({
+    'poly': [Polygon(xy) for xy in m.seattle],
+    'name': [nhood['S_HOOD'] for nhood in m.seattle_info]})
+df_map['area_m'] = df_map['poly'].map(lambda x: x.area)
+df_map['area_km'] = df_map['area_m'] / 100000
+#df_map['poly'].append(Polygon(m2.water[0]))
+
+# <codecell>
+
+"""
+# Plot all neighborhoods
+fig = plt.figure(figsize=(8,13))
+for nhood in m.seattle:
+    xd, yd = zip(*nhood)
+    plt.plot(xd, yd)
+#plt.plot(ld['latitudeE7'], ld['longitudeE7'])
+plt.axis('scaled')
+"""
+
+# <codecell>
+
+# Create Point objects in map coordinates from dataframe lon and lat values
+map_points = pd.Series(
+    [Point(m(mapped_x, mapped_y)) for mapped_x, mapped_y in zip(ld['longitudeE7'], 
+                                                                ld['latitudeE7'])])
+
+plaque_points = MultiPoint(list(map_points.values))
+wards_polygon = prep(MultiPolygon(list(df_map['poly'].values)))
+# calculate points that fall within the London boundary
+ldn_points = filter(wards_polygon.contains, plaque_points)
+
+# <codecell>
+
+# draw ward patches from polygons
+df_map['patches'] = df_map['poly'].map(lambda x: PolygonPatch(
+    x, fc='#555555', ec='#787878', lw=.8, alpha=.9, zorder=0))
+
+plt.clf()
+figwidth = 40
+fig = plt.figure(figsize=(figwidth, figwidth*h/w))
+ax = fig.add_subplot(111, axisbg='w', frame_on=False)
+
+# plot boroughs by adding the PatchCollection to the axes instance
+ax.add_collection(PatchCollection(df_map['patches'].values, match_original=True))
+
+# the mincnt argument only shows cells with a value >= 1
+# hexbin wants np arrays, not plain lists
+hx = m.hexbin(
+    np.array([geom.x for geom in ldn_points]),
+    np.array([geom.y for geom in ldn_points]),
+    gridsize=(70, int(70*h/w)),
+    bins='log',
+    mincnt=1,
+    edgecolor='none',
+    alpha=1.,
+    cmap=plt.get_cmap('Blues'))
+
+# copyright and source data info
+smallprint = ax.text(
+    1.08, 0.0,
+    "Google Latitude data from 2010-2014\nProduced by Tyler Hartley\nInspired by sensitivecities.com",
+    ha='right', va='bottom',
+    size=figwidth/1.75,
+    color='#555555',
+    transform=ax.transAxes)
+
+# Draw a map scale
+m.drawmapscale(
+    coords[0] + 0.05, coords[1] - 0.01,
+    coords[0], coords[1],
+    4.,
+    units='mi',
+    barstyle='fancy', labelstyle='simple',
+    fillcolor1='w', fillcolor2='#555555',
+    fontcolor='#555555',
+    zorder=5)
+
+plt.title("Latitude Location History - All Time")
+#plt.tight_layout()
+# this will set the image width to 722px at 100dpi
+#fig.set_size_inches(7., 10.5)
+#plt.savefig('data/location_history_all.png', dpi=300, alpha=True)
+plt.show()
+
+# <headingcell level=1>
+
+# Scratch
 
 # <codecell>
 
