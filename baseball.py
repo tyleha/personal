57435173e2b6d13dad07989ca0164e1b2ca08f7e
Tyler Hartley <tyleha@gmail.com>
2014-09-02 16:27:25 -0700

    skeletoning

--- baseball.py
+++ baseball.py
@@ -1,11 +1,25 @@
 # -*- coding: utf-8 -*-
 # <nbformat>3.0</nbformat>
 
+# <markdowncell>
+
+# # Baseball salary analysis
+# ## Flow
+# 1. Increase in total league salary over time As compared to a variety of market metrics
+# 2. Team salary graph over time. Point out notable teams. Compare both %spending and #rank
+# 3. The big question - does money buy wins?
+#     1. All year plot of wins (or win%) vs %spending. Draw conclusions
+#     2. Plot of Slope of line from year to year with 95% CI error bars
+#     3. dStd over time - has the spending of money become more or less diverse?
+# 4. Assuming we see a minimal correlation between money and success, what is our hypothesis? Of late - 6 yr team ownership. Plot wins vs salary but color/size dots based on team avg age.
+
 # <codecell>
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
+import wolframalpha as wa
 
 # <codecell>
 
@@ -36,19 +50,6 @@ stats.salary = stats.salary.map(to_float)
 
 # <codecell>
 
-# Let's just look at years where we have salary info
-stats = stats[stats.year > 1976].reset_index(drop=True)
-
-# <codecell>
-
-# Normalize salary to fraction of total money spent that year
-def add_prop(group):
-    group['frac_salary'] = group.salary/group.salary.sum()*100
-    return group
-stats = stats.groupby('year').apply(add_prop)
-
-# <codecell>
-
 # Fix the weird MLB name issues
 def team_groups(name):
     # Edge cases - 
@@ -66,11 +67,75 @@ def team_groups(name):
 
 stats.team = stats.team.map(team_groups)
 
+# <headingcell level=1>
+
+# 1. Total League Salary
+
+# <codecell>
+
+# Let's just look at years where we have salary info
+stats = stats[stats.year > 1976].reset_index(drop=True)
+
+# <codecell>
+
+league_salary = stats.groupby('year').salary.sum()
+
+# <codecell>
+
+ff = pd.ExcelFile(r'C:\Users\thartley\Documents\Dropbox\prices.xlsx')
+incomes = ff.parse('household')
+incomes = incomes[incomes.year > 1976].reset_index(drop=True)
+textbooks = ff.parse('textbooks')
+textbooks = pd.DataFrame([textbooks.ix[i] for i in range(0, len(textbooks), 12)])
+textbooks['year'] = textbooks.month.map(lambda x: x.year)
+textbooks = textbooks[textbooks.year > 1976].reset_index(drop=True)
+
+# <codecell>
+
+plt.figure()
+plt.plot(incomes.year, incomes.nominal)
+plt.plot(textbooks.year, textbooks.CPI*(incomes.nominal.iloc[-1]/textbooks.CPI[0]))
+
+# <codecell>
+
+incomes.nominal.iloc[-1]
+
+# <codecell>
+
+"""
+wolfram_api_id = "G3Y8VX-E83L2YWY59"
+client = wa.Client(wolfram_api_id)
+
+res = client.query('{0} {1} dollars in 2013 dollars'.format(1000, 1977))
+
+for r in res.results:
+    price =  float(re.findall('^\$([\d.]+)', r.text)[0])
+    break
+"""
+
+# <codecell>
+
+fig = plt.figure(figsize=(12,8))
+plt.plot(league_salary.year, league_salary.salary)
+plt.plot()
+
+# <headingcell level=1>
+
+# 2. Team Salary over Time
+
+# <codecell>
+
+# Normalize salary to fraction of total money spent that year
+def add_prop(group):
+    group['frac_salary'] = group.salary/group.salary.sum()*100
+    return group
+stats = stats.groupby('year').apply(add_prop)
+
 # <codecell>
 
 byteam = stats.groupby(by='team')
 for i, g in byteam:
-    color = 'b'
+    color = 'k'
     zorder = None
     alpha = 0.5
     if 'Yankees' in i:
@@ -86,7 +151,6 @@ for i, g in byteam:
 
 # <codecell>
 
-plt.plot?
 
 # <codecell>
 
