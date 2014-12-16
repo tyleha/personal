e61b6ba9e95ad7602cb89c9d71ccb3ab4d09487e
Tyler Hartley <tyler.hartley@csats.com>
2014-12-15 22:44:44 -0800

    working on redditbot in scratch

--- scratch.py
+++ scratch.py
@@ -3,57 +3,24 @@
 
 # <codecell>
 
-def nthfib(n):
-    if n <=1: return n
-    p2 = 0
-    p1 = 1
-    for i in range(n):
-        fib = p2+p1
-        p2 = p1
-        p1 = fib
-    return p1
+import praw
+import time 
 
-nthfib(1)
 
 # <codecell>
 
-xx = np.cos(np.arange(-np.pi, np.pi, 2*np.pi/400))
-plt.plot(xx)
+lookbackTime = time.mktime(time.gmtime()) - 60*60*48# should be sys.argv[1] at some point
+r = praw.Reddit(user_agent='some_kind_of_bot')
 
-# <codecell>
+subredditName = 'python'
+sub = r.get_subreddit(subredditName)
 
-np.savetxt("foo.csv", xx, delimiter=",")
+posts = [p for p in sub.get_new(limit=25)]
+while posts[-1].created_utc > lookbackTime:
+    posts += [p for p in sub.get_new(params={"after":posts[-1].name})]
 
-# <codecell>
-
-def msort3(x):
-    result = []
-    if len(x) < 2:
-        return x
-    mid = int(len(x)/2)
-    y = msort3(x[:mid])
-    z = msort3(x[mid:])
-    i = 0
-    j = 0
-    import pdb;pdb.set_trace()
-    while i < len(y) and j < len(z):
-            if y[i] > z[j]:
-                result.append(z[j])
-                j += 1
-            else:
-                result.append(y[i])
-                i += 1
-    result += y[i:]
-    result += z[j:]
-    return result
-
-# <codecell>
-
-x = [5,3,6,7,1,2,3,9,8,0]
-
-# <codecell>
-
-msort3(x)c
+for p in posts:
+    print p.score, p.title
 
 # <codecell>
 
